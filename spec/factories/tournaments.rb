519c6e2464f2da830df1e42d804547abe161bfbb
Andrew Palmer <andrew.palmer.10@gmail.com>
2021-02-10 12:45:25 -0500

    Update tournaments factory to use block

--- spec/factories/tournaments.rb
+++ spec/factories/tournaments.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :tournament do
     sequence(:name) { |n| "tournament-#{n}" }
     sequence(:pga_id) { |n| "t#{"%03d" % n}" }
-    year 2020
+    year { 2020 }
   end
 end
