030906d05f4f0ec1393e23e448261a3d4ad6c302
Andrew Palmer <andrew.palmer.10@gmail.com>
2021-02-09 21:22:49 -0500

    Update URL Builder to accommodate &s in names

--- spec/services/url_builder_spec.rb
+++ spec/services/url_builder_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe UrlBuilder, type: :service do
   describe 'when the data source is for results' do
     it 'returns a url for the results page using the name of the most-recent tournament with the same pga_id' do
       source = create(:data_source, stat: DataSource::RESULTS, pga_id: 'sourceId')
-      tournament1 = create(:tournament, year: 2015, pga_id: 'tourneyId', name: 'Wrong Name')
-      tournament2 = create(:tournament, year: 2016, pga_id: 'tourneyId', name: 'Also Wrong')
-      tournament3 = create(:tournament, year: 2017, pga_id: 'tourneyId', name: 'Correct & Name')
+      tournament1c = create(:tournament, year: 2015, pga_id: 'tourneyId', name: 'Wrong Name')
+      tournament1b = create(:tournament, year: 2016, pga_id: 'tourneyId', name: 'Also Wrong')
+      tournament1a = create(:tournament, year: 2017, pga_id: 'tourneyId', name: 'Correct & Name')
+      tournament2 = create(:tournament, year: 2018, pga_id: 'differentTourneyId', name: 'AT&T Pebble Beach Pro-Am')
 
-      expect(UrlBuilder.build(source, tournament1)).to eql("https://www.pgatour.com/tournaments/correct-name/past-results/jcr:content/mainParsys/pastresults.selectedYear.2015.html")
-      expect(UrlBuilder.build(source, tournament2)).to eql("https://www.pgatour.com/tournaments/correct-name/past-results/jcr:content/mainParsys/pastresults.selectedYear.2016.html")
-      expect(UrlBuilder.build(source, tournament3)).to eql("https://www.pgatour.com/tournaments/correct-name/past-results/jcr:content/mainParsys/pastresults.selectedYear.2017.html")
-
+      expect(UrlBuilder.build(source, tournament1c)).to eql("https://www.pgatour.com/tournaments/correct-name/past-results/jcr:content/mainParsys/pastresults.selectedYear.2015.html")
+      expect(UrlBuilder.build(source, tournament1b)).to eql("https://www.pgatour.com/tournaments/correct-name/past-results/jcr:content/mainParsys/pastresults.selectedYear.2016.html")
+      expect(UrlBuilder.build(source, tournament1a)).to eql("https://www.pgatour.com/tournaments/correct-name/past-results/jcr:content/mainParsys/pastresults.selectedYear.2017.html")
+      expect(UrlBuilder.build(source, tournament2)).to eql("https://www.pgatour.com/tournaments/at-t-pebble-beach-pro-am/past-results/jcr:content/mainParsys/pastresults.selectedYear.2018.html")
     end
   end
 
